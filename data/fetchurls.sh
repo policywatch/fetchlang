b6543df4de90d2089954ec7761d99836670c01fd
Oleks <oleks@oleks.info>
2019-01-10 14:02:54 +0100

    Add a script to fetch tosback URLs

---
+++ data/fetchurls.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+rm -f master.zip
+
+wget -o wget.log -O master.zip \
+  https://github.com/tosdr/tosback2/archive/master.zip
+
+trap "rm -f master.zip" INT TERM EXIT
+
+rm -rf master
+
+unzip -d master master.zip 2>&1 > unzip.log
+
+trap "rm -rf master" INT TERM EXIT
+
+find master/tosback2-master/rules/ -iname "*.xml" \
+  -exec xmllint {} -xpath "string(//url/@name)" \; \
+  > urls.txt
