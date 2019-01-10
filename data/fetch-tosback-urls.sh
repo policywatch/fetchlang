7c77b6493bcaf17901d5dc173833626ca411ba49
Oleks <oleks@oleks.info>
2019-01-10 15:09:00 +0100

    Ditch the log files

--- data/fetch-tosback-urls.sh
+++ data/fetch-tosback-urls.sh
@@ -6,10 +6,10 @@ tmpdir=$(mktemp -d)
 
 trap "rm -rf $tmpdir" INT TERM EXIT
 
-wget -o wget.log -O $tmpdir/master.zip \
+wget -O $tmpdir/master.zip \
   https://github.com/tosdr/tosback2/archive/master.zip
 
-unzip -d $tmpdir/ $tmpdir/master.zip > unzip.log 2>&1
+unzip -d $tmpdir/ $tmpdir/master.zip
 
 find $tmpdir/tosback2-master/rules/ -iname "*.xml" \
   -exec xmllint {} -xpath "string(//url/@name)" \; \
