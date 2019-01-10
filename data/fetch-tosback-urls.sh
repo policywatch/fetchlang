df36bc2e68c548ffa3ce19707a6739fd7ba4d0eb
Oleks <oleks@oleks.info>
2019-01-10 15:18:47 +0100

    Use quiet mode

--- data/fetch-tosback-urls.sh
+++ data/fetch-tosback-urls.sh
@@ -7,8 +7,8 @@ url=https://github.com/tosdr/tosback2/archive/master.zip
 tmpdir=$(mktemp -d)
 trap "rm -rf $tmpdir" INT TERM EXIT
 
-wget -O $tmpdir/master.zip "${url}"
-unzip -d $tmpdir/ $tmpdir/master.zip
+wget -q -O $tmpdir/master.zip "${url}"
+unzip -q -d $tmpdir/ $tmpdir/master.zip
 
 find $tmpdir/tosback2-master/rules/ -iname "*.xml" \
   -exec xmllint {} -xpath "string(//url/@name)" \; \
